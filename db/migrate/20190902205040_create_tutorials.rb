592d2296dccc252c2d4b88ae30dc7d222aa9e6f4
acsaralegui <aaron.saralegui@gmail.com>
2019-09-26 22:59:30 +0000

    working on migrations

--- db/migrate/20190902205040_create_tutorials.rb
+++ db/migrate/20190902205040_create_tutorials.rb
@@ -2,7 +2,7 @@ class CreateTutorials < ActiveRecord::Migration[5.2]
   def change
     create_table :tutorials do |t|
       t.string :title
-      t.string :System_type
+      #t.string :System_type
       t.text :description
       t.decimal :cost
       t.integer :user_id
